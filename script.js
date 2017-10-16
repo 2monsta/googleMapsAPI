474f6a28f6251bea4551520f16b813c7c9923888
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-10-16 15:43:23 -0400

    click to zoom work

--- script.js
+++ script.js
@@ -41,13 +41,43 @@ function initMap(){
 	// builds the first row of the table
 	function addCityToList(city){
 		var newHtml = `<tr>`;
-			newHtml += `<td class="city-name">${city.city}</td>`;
+			newHtml += `<td class="city-name" index=${city.yearRank - 1}>${city.city}</td>`;
 			newHtml += `<td class="city-state">${city.state}</td>`;
-			newHtml += `<td class="city-directions"><button class="btn btn-primary">Get Directions</button></td>`;
-			newHtml += `<td class="city-zoom"><button class="btn btn-success">Zoom to City</button></td>`;
+			newHtml += `<td class="city-directions" index=${city.yearRank - 1}><button class="btn btn-primary">Get Directions</button></td>`;
+			newHtml += `<td class="city-zoom" index=${city.yearRank - 1}><button class="btn btn-success">Zoom to City</button></td>`;
 			newHtml += `</tr>`;
 		return newHtml;
 	}
+
+	function zoomToCity(lat, lon){
+		// google maps api has a construtor to make latlong object
+		var LL = new google.maps.LatLng(lat, lon);
+		map = new google.maps.Map(document.getElementById("map"), {
+			zoom: 15,
+			center: LL
+		});
+		infoWindow = new google.maps.InfoWindow();
+		// places is a add-on for gogle maps so we can search for stuff
+		console.log(google.maps);
+		var places = new google.maps.places.PlacesService(map);
+		// // placese has a method called "nearbySearch" that we can pass and object
+		// 1. location
+		// 2. radius
+		// 3. types
+		places.nearbySearch({
+			location: LL,
+			radius: 500,
+			type: ["stadium"]
+		}, function(result, status){
+			console.log(result);
+		});
+	}
+	// function triggerClick(ind){
+	// 	// trigger = simulate the given event (click, change, etc);
+	// 	// arg 1: Whwat element
+	// 	var index = $(ind).attr("index");
+	// 	google.maps.event.trigger(markers[ind], "click");
+	// }
 	
 	var markers = [];
 	var infoWindow = new google.maps.InfoWindow({});
@@ -60,7 +90,21 @@ function initMap(){
 		listHTML += addCityToList(city);
 	});
 	$("#cities-table tbody").html(listHTML);
-	// console.log(listHTML);
+
+	$(".city-name").click(function(){
+		console.log("cities clicked on");
+		var index = $(this).attr("index");
+		google.maps.event.trigger(markers[index], "click");
+	})
+	
+	$(".city-zoom").click(function(){
+		var index = $(this).attr("index");
+		zoomToCity(cities[index].lat, cities[index].lon);
+	});
+
+	$(".city-directions").click(function(){
+		var index = $(this).attr("index");
+	});
 
 	$("#filter-form").submit(function(e){
 		e.preventDefault();
